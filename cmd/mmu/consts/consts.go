dd55fe671425ba71903a934eb0b04d76b2b28aab
Harry Wray <harry@dydx.exchange>
2025-01-22 16:50:35 -0500

    Reorder consts

--- cmd/mmu/consts/consts.go
+++ cmd/mmu/consts/consts.go
@@ -11,12 +11,12 @@ const StagingAPIURL = "https://ievd6jluve.execute-api.ap-northeast-1.amazonaws.c
 // Used by external MM Operator to fetch transactions for updating Testnet and Mainnet MMs.
 const ProdAPIURL = "https://fdviqy4mbk.execute-api.ap-northeast-1.amazonaws.com/mainnet/market-map-updater/v1/tx"
 
-// Get list of dYdX networks / chains supported by the MMU pipeline
-func GetSupportedNetworks() []string {
-	return []string{"testnet", "mainnet"}
-}
-
 // Get list of valid envs for the MMU pipeline
 func GetSupportedEnvironments() []string {
 	return []string{"staging", "mainnet"}
+}
+
+// Get list of dYdX networks / chains supported by the MMU pipeline
+func GetSupportedNetworks() []string {
+	return []string{"testnet", "mainnet"}
 }
