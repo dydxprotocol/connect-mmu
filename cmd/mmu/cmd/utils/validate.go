4401420f2cd508a25bc8545499e401b7227a3481
Harry Wray <harry@dydx.exchange>
2025-01-28 16:18:19 -0500

    Update

--- cmd/mmu/cmd/utils/validate.go
+++ cmd/mmu/cmd/utils/validate.go
@@ -358,9 +358,9 @@ func fetchAPIKeysAndWriteToOracleConfig() error {
 
 	for _, provider := range oracleConfig["providers"].(map[string]interface{}) {
 		for _, endpoint := range provider.(map[string]interface{})["api"].(map[string]interface{})["endpoints"].([]interface{}) {
-			url := endpoint.(map[string]string)["url"]
-			fmt.Printf("Setting url: %s, %s", url, apiKeyMap[url])
-			endpoint.(map[string]interface{})["authentication"].(map[string]string)["apiKey"] = apiKeyMap[url]
+			url := endpoint.(map[string]interface{})["url"]
+			fmt.Printf("Setting url: %s, %s", url, apiKeyMap[url.(string)])
+			endpoint.(map[string]interface{})["authentication"].(map[string]string)["apiKey"] = apiKeyMap[url.(string)]
 		}
 	}
 
